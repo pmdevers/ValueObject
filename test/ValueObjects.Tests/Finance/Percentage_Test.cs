9e9094cf3f07211b11c03cc866ad3b369845adb1
Patrick Evers <pmdevers@gmail.com>
2025-12-14 16:58:23 +0100

    hmm

--- test/ValueObjects.Tests/Finance/Percentage_Test.cs
+++ test/ValueObjects.Tests/Finance/Percentage_Test.cs
@@ -7,15 +7,16 @@ namespace ValueObjects.Tests.Finance;
 public class Percentage_Test
 {
     [Theory]
-    [InlineData("50%", "0.5")]
-    [InlineData("100%", "1.0")]
-    [InlineData("0.5", "0.05")]
-    [InlineData("100", "1.0")]
-    [InlineData("120%", "1.2")]
+    [InlineData("50%", "0,5")]
+    [InlineData("100%", "1,0")]
+    [InlineData("0,5", "0,005")]
+    [InlineData("100", "1,0")]
+    [InlineData("120%", "1,2")]
     public void Parse_ShouldParseValidPercentageStrings(string percentage, string expected)
     {
+        var culture = CultureInfo.GetCultureInfo("nl-NL");
         var result = Percentage.Parse(percentage);
-        var expectedValue = decimal.Parse(expected, CultureInfo.InvariantCulture);
+        var expectedValue = decimal.Parse(expected, culture);
 
         (result == expectedValue).Should().BeTrue();
     }
